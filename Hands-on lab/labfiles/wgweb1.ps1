353635eb6c94fe9ba240424c893cd4a0ae9795c9
amal gireesh <48020356+amalgireesh@users.noreply.github.com>
2025-02-19 01:40:56 +0530

    Update wgweb1.ps1

--- Hands-on lab/labfiles/wgweb1.ps1
+++ Hands-on lab/labfiles/wgweb1.ps1
@@ -1,17 +1,3 @@
-Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
-
-#Import Common Functions
-$path = pwd
-$path=$path.Path
-$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
-. $commonscriptpath
-
-# Run Imported functions from cloudlabs-windows-functions.ps1
-WindowsServerCommon 
-InstallEdgeChromium 
-
 # Install IIS if not already installed
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools
 Write-Host "IIS installed successfully."
