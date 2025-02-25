f949e86b812fd81605cbd2e4e22755619f8b1590
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2025-02-25 19:41:41 +0530

    update

---
+++ Hands-on lab/labfiles/wgsql-logontask.ps1
@@ -0,0 +1,25 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\logontasklogs.txt -Append
+
+Import-Module SqlServer
+$databaseName = "AdventureWorks2017"
+$backupFilePath = "C:\LabFiles\AdventureWorks\AdventureWorks2017.bak"
+
+$username = "demouser"
+$password = "demo@pass123"
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
+
+
+# Restore the database from the downloaded backup file
+$restorePath = "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA"
+Restore-SqlDatabase -ServerInstance "WGSQL1" -Database "AdventureWorks2017" -BackupFile "C:\LabFiles\AdventureWorks\AdventureWorks2017.bak" -RelocateFile @(
+    @{LogicalFileName = "AdventureWorks2017"; PhysicalFileName = "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\AdventureWorks2017.mdf"},
+    @{LogicalFileName = "AdventureWorks2017_log"; PhysicalFileName = "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\AdventureWorks2017.ldf"}
+) -ReplaceDatabase -SqlCredential $cred
+
+
+#Remove logontask file from labfiles
+Remove-Item 'C:\LabFiles\wgsql-logontask.ps1' -Recurse -force
+
+Stop-Transcript
+Restart-Computer -Force
